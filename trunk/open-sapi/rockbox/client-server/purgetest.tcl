69809e2b90630733c7be52842abba072dcc4fd89
thomaslloyd82 <thomaslloyd82@9a8f70e2-b651-11dd-9d36-1d0c09c95ed7>
2010-03-28 22:37:13 +0000

    Added bookmarks to test event monitoring

--- trunk/open-sapi/rockbox/client-server/purgetest.tcl
+++ trunk/open-sapi/rockbox/client-server/purgetest.tcl
@@ -6,7 +6,7 @@ set maxint [expr 0x7[string range [format %X -1] 1 end]]
        set voice [::tcom::ref createobject Sapi.SpVoice]
        
 puts [ time {
-       $voice Speak "This is a test of the ms speech engine time cut out." 1
+       $voice Speak "<bookmark mark="bookmark_one"/>This is a test of the ms speech<bookmark mark="bookmark_two"/> engine time cut out.<bookmark mark="bookmark_three"/>" 1
        after 1000 { $voice Speak " " 3 }
        } 50
        
